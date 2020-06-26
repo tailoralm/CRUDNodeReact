963be7b28bdfd4c06c9de0a6a26dddaa9d1e9c1b
Tailor Maffini <tailor.alm@gmail.com>
2020-06-25 23:50:02 -0300

    First Commit

---
+++ node-api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
+
+//iniciando o app
+const app = express();
+
+//iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true});
+
+requireDir('./src/models');
+
+// const Product = mongoose.model('Product');
+
+//Rotas
+app.use('/api', require("./src/routes"));
+ 
+app.listen(3001);
