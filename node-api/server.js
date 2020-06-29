f33e277222e6cd74a1d1c509a549aeb72e67192c
Tailor Maffini <tailor.alm@gmail.com>
2020-06-28 23:48:27 -0300

    finish node api

--- node-api/server.js
+++ node-api/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 //iniciando o app
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 //iniciando o DB
-mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useUnifiedTopology: true, useNewUrlParser: true});
 
 requireDir('./src/models');
 
